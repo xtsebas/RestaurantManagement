3b7f67a748682f26b21dd969f4e8f3799dd2c555
josuemj <josuemj456@gmail.com>
2024-04-11 20:49:08 -0600

    login taskflow created

--- controller.py
+++ controller.py
@@ -15,3 +15,23 @@ def validar_mesa(no_mesa, area_id, connection):
     except (Exception, psycopg2.Error) as error:
         print("Error al obtener mesas:", error)
         return []
+
+def mesa_disponible(no_mesa, connection):
+    try:
+        cursor = connection.cursor()
+        query = "SELECT estado FROM mesa WHERE no_mesa = %s;"
+        cursor.execute(query, (no_mesa,))  # Pass no_mesa as a parameter
+
+        row = cursor.fetchone()  # Assuming each 'no_mesa' has exactly one row
+        cursor.close()
+        estado = row[0]
+        print("estado: "+str(estado))
+
+        if estado == False: #Si la mesa no esta ocupada
+            print("Mesa libre")
+        else:
+            print("La mesa se encuetra ocupada o olvido cerrar la cuenta")
+        
+    except (Exception, psycopg2.Error) as error:
+        print("Error con la mesa", error)
+        return False  # Return False on error or handle as needed
--- restaurante.py
+++ restaurante.py
@@ -3,16 +3,21 @@ from tabulate import tabulate
 import os
 from controller import *
 
+"""
+Funciones relacionadas con el flujo del restaurante
+"""
 def ordernar():
     return None
 
-def abrirCuenta():
+def abrirCuenta(no_mesa,connection):
+    if mesa_disponible(no_mesa,connection):
+        print("Cuenta abierta exitosamente")
     return None
 
 def cerrarCuenta():
     return None
 
-def opcionesMesa():
+def opcionesMesa(no_mesa,connection):
     print("================================")
     print("Acciones con la mesa")
     print("1. Abrir cuenta")
@@ -26,7 +31,7 @@ def opcionesMesa():
         opcionMesa = str(input("Ingresa accion con la mesa: "))
 
         if opcionMesa == "1":
-            abrirCuenta()
+            abrirCuenta(no_mesa, connection)
         elif opcionMesa == "2":
             cerrarCuenta()
         elif opcionMesa == "3":
@@ -49,7 +54,7 @@ def accionMesa(area,connection):
         
         if len(validar_mesa(no_mesa=no_mesa, area_id=area,connection=connection)) == 1:
             print("Valid")
-            opcionesMesa()
+            opcionesMesa(no_mesa,connection)
         else:
             print("Mesa no esta en esta area")
 
@@ -88,67 +93,97 @@ def mesas(database):
     elif opcion=='3':
         pass
     
+"""
+Funciones relacionadas con el inicio de sesion
+"""
 
+"""
+Funcion general del manejor del restaurante
+"""
+def restaurante(database):
+
+    while True:
+        
+        print("\n=====================")
+        print("Manejo de restaurante")
+        print("=====================")
+        print("1. Pantalla de Mesas")
+        print("2. Pantalla de cocina")
+        print("3. Pantalla de bar")
+        print("4. Reportes")
+        print("10. Cerrar sesion")
+
+        accion = str(input("Ingrese la pantalla: "))
+        #Limpia la consola
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        if accion == "1":
+            mesas(database=database)
+
+        elif accion == "2":
+            print("\nPantalla de cocina")
+            pantalla_cocina = obtener_cocina(database)
+            headers = ["Cuenta", "Cantidad", "Plato", "Fecha"]
+            print(tabulate(pantalla_cocina, headers=headers))
+            
+        elif accion == "3":
+            print("\nPantalla de bar")
+            pantalla_bar = obtener_bar(database)
+            headers = ["Cuenta", "Cantidad", "Bebida", "Fecha"]
+            print(tabulate(pantalla_bar, headers=headers))
+
+        elif accion == "4":
+            print("\nReportes")
+            print("\n1. Platos mas pedidos por clientes\n2. Horario donde se ingresan mas pedidos\n3. Promedio de tiempo que tardan los clientes en comer\n4. Quejas agrupadas por persona\n5. Quejas agrupadas por item\n6. Eficiencia de los meseros por sus encuestas")
+            reporte = str(input("Ingrese la pantalla: "))
+            
+            if reporte=="1":
+                print("1")
+                
+            elif reporte=="2":
+                print("1")
+                
+            elif reporte=="3":
+                print("1")
+                
+            elif reporte=="4":
+                print("1")
+                
+            elif reporte=="5":
+                print("1")
+                
+            elif reporte=="6":
+                print("1")
+        
+            
+            """
+            menu = obtener_menu(database)
+            headers = ["ID", "Nombre", "Descripción", "Precio", "Tipo"]
+
+            print(tabulate(menu, headers=headers))
+            """
+
+        elif accion == "10":
+            break
+
+
+#Flujo de sign in/ log in
 
 database = conectar_bd()
 
 while True:
-
-    print("\n=====================")
-    print("Manejo de restaurante")
-    print("=====================")
-    print("1. Pantalla de Mesas")
-    print("2. Pantalla de cocina")
-    print("3. Pantalla de bar")
-    print("4. Reportes")
-
-    accion = str(input("Ingrese la pantalla: "))
-    #Limpia la consola
-    os.system('cls' if os.name == 'nt' else 'clear')
+    print("============ Restaurant managment ===========")
+    print("1. Iniciar sesicion\n2. Registrarse\n3. Salir")
+    accion = str(input("Ingrese su opcion ---> "))
 
     if accion == "1":
-        mesas(database=database)
-
+        #opciones de login
+        restaurante(database) #al logear muestra restaurante
     elif accion == "2":
-        print("\nPantalla de cocina")
-        pantalla_cocina = obtener_cocina(database)
-        headers = ["Cuenta", "Cantidad", "Plato", "Fecha"]
-        print(tabulate(pantalla_cocina, headers=headers))
-        
+        #registro
+        pass
     elif accion == "3":
-        print("\nPantalla de bar")
-        pantalla_bar = obtener_bar(database)
-        headers = ["Cuenta", "Cantidad", "Bebida", "Fecha"]
-        print(tabulate(pantalla_bar, headers=headers))
+        break
 
-    elif accion == "4":
-        print("\nReportes")
-        print("\n1. Platos mas pedidos por clientes\n2. Horario donde se ingresan mas pedidos\n3. Promedio de tiempo que tardan los clientes en comer\n4. Quejas agrupadas por persona\n5. Quejas agrupadas por item\n6. Eficiencia de los meseros por sus encuestas")
-        reporte = str(input("Ingrese la pantalla: "))
-        
-        if reporte=="1":
-            print("1")
-            
-        elif reporte=="2":
-            print("1")
-            
-        elif reporte=="3":
-            print("1")
-            
-        elif reporte=="4":
-            print("1")
-            
-        elif reporte=="5":
-            print("1")
-            
-        elif reporte=="6":
-            print("1")
-    
-        
-        """
-        menu = obtener_menu(database)
-        headers = ["ID", "Nombre", "Descripción", "Precio", "Tipo"]
 
-        print(tabulate(menu, headers=headers))
-        """
 
